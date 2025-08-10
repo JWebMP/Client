a370fa7a30735820d2b48311e527dd19fb67ab36
GedMarc <ged_marc@hotmail.com>
2025-08-10 21:51:50 +0200

    marker -> before web structuring

--- src/main/java/com/jwebmp/core/services/IPageConfigurator.java
+++ src/main/java/com/jwebmp/core/services/IPageConfigurator.java
@@ -8,16 +8,14 @@ import com.guicedee.guicedinjection.interfaces.IServiceEnablement;
  * A service for configuration built pages
  */
 public interface IPageConfigurator<J extends IPageConfigurator<J>>
-		extends IDefaultService<J>, IServiceEnablement<J>
+        extends IDefaultService<J>, IServiceEnablement<J>
 {
-	/**
-	 * Configures the given page for the parameters
-	 *
-	 * @param page
-	 * 		The page incoming
-	 *
-	 * @return The original page incoming or a new page, never null
-	 */
-	
-	IPage<?> configure(IPage<?> page);
+    /**
+     * Configures the given page for the parameters
+     *
+     * @param page The page incoming
+     * @return The original page incoming or a new page, never null
+     */
+
+    IPage<?> configure(IPage<?> page);
 }
