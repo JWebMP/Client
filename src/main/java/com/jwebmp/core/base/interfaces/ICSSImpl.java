fc08f46aaacdbc5beec42579791ff767ea2c1147
GedMarc <ged_marc@hotmail.com>
2025-09-09 01:24:02 +0200

    marker -> angular dto updates

--- src/main/java/com/jwebmp/core/base/interfaces/ICSSImpl.java
+++ src/main/java/com/jwebmp/core/base/interfaces/ICSSImpl.java
@@ -1,5 +1,9 @@
 package com.jwebmp.core.base.interfaces;
 
+/**
+ * Client-facing CSS implementation interface.
+ * Methods should return client interfaces, not core implementation classes.
+ */
 public interface ICSSImpl
 {
 
